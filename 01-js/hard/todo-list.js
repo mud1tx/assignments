76b3dd441cdf55e1a2b74decafeb5708c2090e71
mud1tx <muditagarwalna@gmail.com>
2023-12-11 21:09:51 +0530

    completed todo assignment

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,34 @@
 */
 
 class Todo {
-
+  constructor() {
+    this.result = [];
+  }
+  add(par) {
+    this.result.push(par);
+  }
+  remove(index) {
+    this.result.splice(index, 1);
+  }
+  update(ind, todo) {
+    if (ind >= this.result.length) {
+    } else {
+      this.result[ind] = todo;
+    }
+  }
+  getAll() {
+    return this.result;
+  }
+  get(ind) {
+    if (ind >= this.result.length) {
+      return null;
+    } else {
+      return this.result[ind];
+    }
+  }
+  clear() {
+    this.result = [];
+  }
 }
 
 module.exports = Todo;
