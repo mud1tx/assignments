99f973dbeb5250a45705a0737027d1ea2ca533c4
mud1tx <muditagarwalna@gmail.com>
2023-12-11 21:09:34 +0530

    completed Calculator assignment

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -1,21 +1,80 @@
 /*
-  Implement a class `Calculator` having below methods
-    - initialise a result variable in the constructor and keep updating it after every arithmetic operation
-    - add: takes a number and adds it to the result
-    - subtract: takes a number and subtracts it from the result
-    - multiply: takes a number and multiply it to the result
-    - divide: takes a number and divide it to the result
-    - clear: makes the `result` variable to 0
-    - getResult: returns the value of `result` variable
-    - calculate: takes a string expression which can take multi-arithmetic operations and give its result
-      example input: `10 +   2 *    (   6 - (4 + 1) / 2) + 7`
-      Points to Note: 
-        1. the input can have multiple continuous spaces, you're supposed to avoid them and parse the expression correctly
-        2. the input can have invalid non-numerical characters like `5 + abc`, you're supposed to throw error for such inputs
+    Implement a class `Calculator` having below methods
+      - initialise a result variable in the constructor and keep updating it after every arithmetic operation
+      - add: takes a number and adds it to the result
+      - subtract: takes a number and subtracts it from the result
+      - multiply: takes a number and multiply it to the result
+      - divide: takes a number and divide it to the result
+      - clear: makes the `result` variable to 0
+      - getResult: returns the value of `result` variable
+      - calculate: takes a string expression which can take multi-arithmetic operations and give its result
+        example input: `10 +   2 *    (   6 - (4 + 1) / 2) + 7`
+        Points to Note: 
+          1. the input can have multiple continuous spaces, you're supposed to avoid them and parse the expression correctly
+          2. the input can have invalid non-numerical characters like `5 + abc`, you're supposed to throw error for such inputs
 
-  Once you've implemented the logic, test your code by running
-*/
+    Once you've implemented the logic, test your code by running
+  */
 
-class Calculator {}
+const input = "10 +   2 *    (   6 - (4 + 1) / 0) + 7";
+
+class Calculator {
+  constructor() {
+    this.result = 0;
+  }
+
+  add(number) {
+    this.result += number;
+  }
+
+  subtract(number) {
+    this.result -= number;
+  }
+
+  multiply(number) {
+    this.result *= number;
+  }
+
+  divide(number) {
+    if (number === 0) {
+      throw new Error("Cannot divide by zero");
+    }
+    this.result /= number;
+  }
+
+  clear() {
+    this.result = 0;
+  }
+
+  getResult() {
+    return this.result;
+  }
+
+  calculate(expression) {
+    const sanitizedExpression = expression.replace(/\s+/g, " ").trim();
+
+    if (!/^[\d\s\+\-\*\/\(\)\.]+$/.test(sanitizedExpression)) {
+      throw new Error("Invalid characters in the expression");
+    }
+
+    try {
+      const tokens = sanitizedExpression.match(/[\d\.]+|\S/g) || [];
+
+      for (const token of tokens) {
+        if (token === "/") {
+          const nextToken = tokens[tokens.indexOf(token) + 1];
+          if (nextToken === "0") {
+            throw new Error("Cannot divide by zero");
+          }
+        }
+      }
+
+      this.result = eval(sanitizedExpression);
+      return this.result;
+    } catch (error) {
+      throw new Error("Invalid expression");
+    }
+  }
+}
 
 module.exports = Calculator;
